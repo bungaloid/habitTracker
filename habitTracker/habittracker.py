e29851a6300bc70d21245b72a3b9e2110d690151
Shihab W <110503117+bungaloid@users.noreply.github.com>
2024-01-26 14:51:59 -0500

    First time adding the project onto the repo

---
+++ habitTracker/habittracker.py
@@ -0,0 +1,86 @@
+goodHabits = []
+badHabits = []
+
+class Habit:
+    def __init__(self, habitName, habitDescription):
+
+
+def selection():
+    selectionLoop = True
+    while(selectionLoop):
+        selectionChoice = int(input("1: Add Habit\n2: Remove Habit\n3: Check Habits\n4: Quit Program\n"))
+        if(selectionChoice == 1):
+            print("-" * 30)
+            addHabit()
+        elif(selectionChoice == 2):
+            print("-" * 30)
+            removeHabit()
+        elif(selectionChoice == 3):
+            print("-" * 30)
+            print("Good Habits:", goodHabits)
+            print("Bad Habits:", badHabits)
+            print("-" * 30)
+        elif(selectionChoice == 4):
+            selectionLoop = False
+            return
+        else:
+            print("-" * 30)
+            print("Please enter a valid option")
+            print("-" * 30)
+
+def addHabit():
+    habitMenuLoop = True
+    while(habitMenuLoop):
+        habitChoice = int(input("What habit would you like to add?\n1: Good Habit\n2: Bad Habit\n3: Return to Menu\n"))
+        if(habitChoice == 1):
+            print("-" * 30)
+            goodHabits.append(input("Enter a good habit: "))
+        elif(habitChoice == 2):
+            print("-" * 30)
+            badHabits.append(input("Enter a bad habit: "))
+        elif(habitChoice == 3):
+            print("-" * 30)
+            habitMenuLoop = False
+            return
+        else:
+            print("-" * 30)
+            print("Please enter a valid option")
+            print("-" * 30)
+            continue
+        addNewHabit = int(input("Would you like to add another habit?\n1: Yes\n2: No\n"))
+        if(addNewHabit == 1):
+            print("-" * 30)
+            continue
+        else:
+            print("-" * 30)
+            habitMenuLoop = False
+
+def removeHabit():
+    removeChoice = int(input("What kind of habit would you like to remove?\n1: Good Habit\n2: Bad Habit\n"))
+    print(30 * "-")
+    if(removeChoice == 1):
+        habitList = goodHabits
+    elif(removeChoice == 2):
+        habitList = badHabits
+    else:
+        print("Invalid choice")
+        print("Returning to menu")
+        print(30 * "-")
+        return
+    print("Which habit would you like to remove?")
+    for i in range(0,len(habitList)):
+        print(str(i + 1) + ": " + str(habitList[i]))
+    removeIndex = int(input()) - 1
+    if (removeIndex < 0) or (removeIndex > len(habitList)):
+        print("Invalid choice")
+        print("Returning to menu")
+    else:
+        habitList.remove(habitList[removeIndex])
+        print(30 * "-")
+
+
+    
+
+
+selection()
+
